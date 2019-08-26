13c507429f91e050e5c524ea96f0eafb127c5d50
lei <1022958177@qq.com>
2019-08-26 21:04:18 +0800

    [add]  7.整数反转

--- LeetCode/LeetCode/main.cpp
+++ LeetCode/LeetCode/main.cpp
@@ -1,23 +1,9 @@
 #include"removeElement.h"
 #include"findMedianSortedArrays.h"
-
+#include"reverse.h"
 int main()
 {
-    findMedianSortedArrays Find;
-    vector<int> test;
-    vector<int> test2;
-    test2.push_back(2);
-    //test2.push_back(4);
-    //test2.push_back(6);
-    int num;
-    double dou1;
-    removeElement RemoveElement;
-    test.push_back(1);
-    test.push_back(3);
-    //test.push_back(3);
-    //test.push_back(5);
-    //num = RemoveElement.Solution(test,1);
-    dou1 = Find.Solution(test, test2);
-    getchar();
+    reverse R;
+    int num = R.Solution(1534236469);
     return 0;
 }
---
+++ LeetCode/LeetCode/reverse.h
@@ -0,0 +1,29 @@
+#pragma once
+#include"config.h"
+
+class reverse 
+{
+public:
+    int Solution(int x)
+    {
+        // 41 -> 1,4
+        vector<int> num;
+        long long number = x;
+        while (number)
+        {
+            num.push_back(number % 10);
+            number = number / 10;
+        }
+        number = 0;
+        for (int i = 0; i < num.size(); i++)
+        {
+            number = number * 10 + num[i];
+        }
+        //num.~vector();
+        if ((number > INT32_MAX) || (number < INT32_MIN))
+        {
+            number = 0;
+        }
+        return number;
+    }
+};
