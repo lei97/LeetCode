6c5a8c006f5c69fbcd02bcb633821c1575bb89db
lei <1022958177@qq.com>
2019-08-29 22:34:03 +0800

    [add]  292.Nim游戏

---
+++ LeetCode/LeetCode/canWinNim.h
@@ -0,0 +1,16 @@
+#pragma once
+#include"config.h"
+
+class canWinNim
+{
+public:
+    bool Solution(int n)
+    {
+        int data = n;
+        if (n < 3)
+        {
+            return true;
+        }
+        //
+    }
+};
