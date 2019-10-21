d660076bd965598fd3489541798784c55f2b5b59
lei <1022958177@qq.com>
2019-10-21 20:16:05 +0800

    创建 238. 除自身以外数组的乘积

---
+++ LeetCode/productExceptSelf.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "config.h"
+
+//问题： 给定长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，
+//其中 output[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积。
+//输入：[1,2,3,4]
+//输出：[24,12,8,6]
+//要求：请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+//进阶：在常数空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。）
+
+class productExceptSelf {
+public:
+	vector<int> source(vector<int>& nums)
+	{
+		;
+	}
+
+	void test()
+	{
+
+	}
+
+private:
+	vector<int> m_data;
+};
