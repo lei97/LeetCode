28c10253bff3efa57826ed350b9c5b57e08ffb88
lei <1022958177@qq.com>
2019-12-01 21:20:05 +0800

    创建 singleNumber

--- LeetCode/config.h
+++ LeetCode/config.h
@@ -5,6 +5,7 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include <cassert>
 
 struct ListNode {
     int val;
---
+++ LeetCode/singleNumber.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "config.h"
+#include <vector>
+
+using std::vector;
+
+class SingleNumber {
+public:
+	/*	
+		简介：给定一个非空整数数组，除了某个元素只出现一次以外，
+		其余每个元素均出现两次。找出那个只出现了一次的元素。
+		要求：应该具有线性时间复杂度。 可以不使用额外空间来实现
+	*/
+	int solution(std::vector<int>& nums)
+	{
+		/*
+			解题思路：
+				1. 
+				2.
+		*/
+		int size = nums.size();
+		if (size <= 1)
+		{
+			return nums[0];
+		}
+
+		int tmp = nums[0];
+		for (int i = 1; i < size; i++)
+		{
+			tmp == nums[i];
+		}
+	}
+
+	/*
+		测试案例：
+			1. [2, 2, 1]  -> 1
+			2. [4,1,2,1,2] -> 4
+	*/
+	bool test()
+	{
+		vector<int> num1{2,2,1};		
+		assert(solution(num1) != 1);
+
+		vector<int> num2{ 4, 1, 2, 1, 2 };
+		assert(solution(num2) != 4);
+	}
+};
