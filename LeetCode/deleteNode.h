7612390ef81d4f24992f92ef75f08f0bde4346f5
lei <1022958177@qq.com>
2019-12-27 22:15:50 +0800

    add  237. 删除节点

---
+++ LeetCode/deleteNode.h
@@ -0,0 +1,19 @@
+#pragma once
+#include"config.h"
+
+class deleteNode
+{
+    /*
+        使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+        现有一个链表 -- head = [4,5,1,9]，
+        输入: head = [4,5,1,9], node = 5
+        输出: [4,1,9]
+        思路：跳过node节点
+    */
+    void source(ListNode* node)
+    {
+        ListNode* p = node->next;
+        node->val = p->val;
+        node->next = p->next;
+    }
+};
