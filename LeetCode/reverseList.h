6b8bc890a1e46194f21ec15decb5e9851524a0ce
lei <1022958177@qq.com>
2021-01-07 14:31:13 +0800

    更新反转链表算法

--- LeetCode/reverseList.h
+++ LeetCode/reverseList.h
@@ -6,25 +6,42 @@ class reverseList {
 public:
     ListNode* Solution(ListNode* head) 
     {
-        ListNode* inNode = head;
-        ListNode* tmpNode = nullptr;
-        while (inNode)
+        // 方式1
+
+        //ListNode* inNode = head;
+        //ListNode* tmpNode = nullptr;
+        //while (inNode)
+        //{
+        //    ListNode* tmp = new ListNode(inNode->val);
+        //    tmp->next = tmpNode;
+        //    tmpNode = tmp;
+        //    //if (tmpNode->next != nullptr)
+        //    //{
+        //    //    tmp->next = tmpNode;
+        //    //    tmpNode = tmp;
+        //    //}
+        //    //else
+        //    //{
+        //    //    tmp->next = tmpNode;
+        //    //    tmpNode = tmp;
+        //    //}
+        //    inNode = inNode->next;
+        //}
+        //return tmpNode;
+
+        // 方式2
+        ListNode* front = new ListNode(0);
+
+        while (head)
         {
-            ListNode* tmp = new ListNode(inNode->val);
-            tmp->next = tmpNode;
-            tmpNode = tmp;
-            //if (tmpNode->next != nullptr)
-            //{
-            //    tmp->next = tmpNode;
-            //    tmpNode = tmp;
-            //}
-            //else
-            //{
-            //    tmp->next = tmpNode;
-            //    tmpNode = tmp;
-            //}
-            inNode = inNode->next;
+            ListNode* tmp = head->next;
+            head->next = front->next;
+            front->next = head;           
+            head = tmp;
         }
-        return tmpNode;
+
+        ListNode* tmp = front->next;
+        delete front;
+        return tmp;
     }
 };
